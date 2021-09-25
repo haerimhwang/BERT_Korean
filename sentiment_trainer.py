6c505a41d70274f44e7c3b74e922e0860c08354b
haerimhwang <75721007+haerimhwang@users.noreply.github.com>
2021-09-25 21:54:07 +0900

    Update sentiment_trainer.py

--- sentiment_trainer.py
+++ sentiment_trainer.py
@@ -159,6 +159,19 @@ class FineTuning:
         np.random.seed(seed_val)
         torch.manual_seed(seed_val)
         torch.cuda.manual_seed_all(seed_val)
+        
+        ############################################################################
+        # 참고 자료: https://blog.naver.com/PostView.naver?blogId=vail131&logNo=222306329719&parentCategoryNo=&categoryNo=21&viewDate=&isShowPopularPosts=true&from=search
+        random.seed(seed_val) #파이썬 자체 모듈 random 모듈의 시드 고정
+        np.random.seed(seed_val) #numpy를 사용할 경우 고정
+        torch.manual_seed(seed_val) #torch를 거치는 모든 난수들의 생성순서를 고정한다
+        torch.cuda.manual_seed_all(seed_val)  # if use multi-GPU
+        
+        torch.cuda.manual_seed(seed_val) #cuda를 사용하는 메소드들의 난수시드는 따로 고정해줘야한다 
+        torch.backends.cudnn.deterministic = True #딥러닝에 특화된 CuDNN의 난수시드도 고정 
+        torch.backends.cudnn.benchmark = False
+        ############################################################################
+        
         training_log = []
         desc_training_loss = None
         self.bert.train()
